e4a6403374bde230123f163aba145abdd555b647
kati <wi16b141@technikum-wien.at>
2017-10-08 20:57:20 +0200

    1. Commit

---
+++ SimpleToDoList/Models/ToDo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleToDoList.Models
+{
+    public class ToDo
+    {
+        public string Description { get; set; }
+        public bool isDone { get; set; }
+
+        public ToDo(string description, bool isDone)
+        {
+            Description = description;
+            this.isDone = isDone;
+        }
+
+    }
+}
---
+++ SimpleToDoList/ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleToDoList.ViewModels
+{
+    public class MainViewModel
+    {
+        public string newTaskDescription { get; set; }
+        public ObservableCollection<string> toDoList { get; set; } = new ObservableCollection<string>();
+
+        public MainViewModel()
+        {
+            toDoList.Add("Some new Task");
+        }
+    }
+}
